54b549feaebfd666a3f32653a47ed4628c97b8c1
bassemshili3-cpu <bassem.shili3@gmail.com>
2026-03-02 00:17:11 +0100

    1.1

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,12 +2,12 @@ import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    { url: 'https://ohmyjob.com', lastModified: new Date() },
-    { url: 'https://ohmyjob.com/jobs', lastModified: new Date() },
-    { url: 'https://ohmyjob.com/privacy', lastModified: new Date() },
-    { url: 'https://oh-my-job.com/jobs-for-14-year-olds', lastModified: new Date() },
-    { url: 'https://oh-my-job.com/jobs-for-15-year-olds', lastModified: new Date() },
-    { url: 'https://oh-my-job.com/jobs-for-16-year-olds', lastModified: new Date() },
-    { url: 'https://oh-my-job.com/fifo-jobs', lastModified: new Date() },
+    { url: 'https://www.oh-my-job.com', lastModified: new Date() },
+    { url: 'https://www.oh-my-job.com/jobs', lastModified: new Date() },
+    { url: 'https://www.oh-my-job.com/privacy', lastModified: new Date() },
+    { url: 'https://www.oh-my-job.com/jobs-for-14-year-olds', lastModified: new Date() },
+    { url: 'https://www.oh-my-job.com/jobs-for-15-year-olds', lastModified: new Date() },
+    { url: 'https://www.oh-my-job.com/jobs-for-16-year-olds', lastModified: new Date() },
+    { url: 'https://www.oh-my-job.com/fifo-jobs', lastModified: new Date() },
   ]
 }
