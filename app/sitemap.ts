74518f59c942d454e8c513f976d87551bb833d12
bassemshili3-cpu <bassem.shili3@gmail.com>
2026-03-02 01:20:43 +0100

    1.5

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,12 +2,47 @@ import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    { url: 'https://www.oh-my-job.com', lastModified: new Date() },
-    { url: 'https://www.oh-my-job.com/jobs', lastModified: new Date() },
-    { url: 'https://www.oh-my-job.com/privacy', lastModified: new Date() },
-    { url: 'https://www.oh-my-job.com/jobs-for-14-year-olds', lastModified: new Date() },
-    { url: 'https://www.oh-my-job.com/jobs-for-15-year-olds', lastModified: new Date() },
-    { url: 'https://www.oh-my-job.com/jobs-for-16-year-olds', lastModified: new Date() },
-    { url: 'https://www.oh-my-job.com/fifo-jobs', lastModified: new Date() },
+    {
+      url: 'https://www.oh-my-job.com',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: 'https://www.oh-my-job.com/jobs',
+      lastModified: new Date(),
+      changeFrequency: 'hourly', // les offres changent souvent
+      priority: 0.9,
+    },
+    {
+      url: 'https://www.oh-my-job.com/jobs-for-14-year-olds',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.oh-my-job.com/jobs-for-15-year-olds',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.oh-my-job.com/jobs-for-16-year-olds',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.oh-my-job.com/fifo-jobs',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.oh-my-job.com/privacy',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.3,
+    },
   ]
 }
