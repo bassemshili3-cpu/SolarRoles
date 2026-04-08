4102ab7c00eabec11f66f328de47d73cd4967ba1
bassemshili3-cpu <bassem.shili3@gmail.com>
2026-04-08 14:00:02 +0200

    24.2

---
+++ app/robots.ts
@@ -0,0 +1,16 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/dashboard/', '/auth/'],
+      },
+    ],
+    sitemap: [
+      'https://www.oh-my-job.com/sitemap.xml',
+    ],
+  }
+}
